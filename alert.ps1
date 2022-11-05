2f511a1e1457a4ea91c613fca74714626afa7660
bissgit <46567410+bissgit@users.noreply.github.com>
2022-11-05 10:39:02 +0000

    Update alert.ps1

--- alert.ps1
+++ alert.ps1
@@ -1,1 +1,1 @@
-Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('THIS IS A TEST TO SHOW POWERSHELL SCRIPT EXECUTION','WARNING')
+Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('THIS IS A TEST TO SHOW POWERSHELL SCRIPT DOWNLOAD AND EXECUTION','WARNING')
